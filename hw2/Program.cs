c0ee33a90d8377477808b66226080cf9d66aab95
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-03-04 02:37:22 +0300

    hw2

---
+++ hw2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число");
+int val =Convert.ToInt32(Console.ReadLine());
+int res;
+if (val< 99)
+ Console.WriteLine ("третьей цифры нет");
+ else if (val > 99 && val <1000)
+{
+  res = (val % 100) % 10 ;
+  Console.WriteLine (res);
+}
